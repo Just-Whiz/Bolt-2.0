3a5952a86f2f9f6f29593a44908d3143f777a1df
Whiz <christiansuy25@gmail.com>
2026-05-09 14:17:24 -0400

    Set up log handler in the main & added aiohttp to the requirements lib

--- main.py
+++ main.py
@@ -26,3 +26,32 @@ LOG_SHEET_NAME = os.getenv("LOG_SHEET_NAME", "Garde Nationale Test Example")
 BLOXLINK_API_KEY = os.getenv("BLOXLINK_API_KEY")
 ROBLOX_OPEN_CLOUD = os.getenv("ROBLOX_OPEN_CLOUD_KEY")
 ROBLOX_GROUP_ID = os.getenv("ROBLOX_GROUP_ID", "772916015")
+
+VERIFIED_USERS_PATH = "verified_users.json"
+
+# Logging setup
+LOG_FILE = "bolt.log"
+file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8", mode="a")
+file_handler.setFormatter(logging.Formatter(
+    fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S"
+))
+bolt_log = logging.getLogger("bolt")
+bolt_log.setLevel(logging.DEBUG)
+bolt_log.addHandler(file_handler)
+bolt_log.propagate = False
+
+for lib in ("discord", "discord.http", "discord.gateway", "gspread", "google"):
+    lib_logger = logging.getLogger(lib)
+    lib_logger.setLevel(logging.DEBUG)
+    lib_logger.addHandler(file_handler)
+    lib_logger.propagate = False
+
+handler = file_handler
+
+# Google Spreadsheet website scopes
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets"
+    "https://www.googleapis.com/auth/drive"
+]
+
